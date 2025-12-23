9bce3bf665ee69f0a51e11b4ffeac512f23b121a
rasadin <12105708+rasadin@users.noreply.github.com>
2025-12-23 23:19:59 +0600

    setup

--- wp-config.php
+++ wp-config.php
@@ -20,7 +20,7 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', '4sixdb' );
+define( 'DB_NAME', '4sixdb_paul' );
 
 /** Database username */
 define( 'DB_USER', 'root' );
